37d62e71ff0f456e0c7cb6ca4a3a6417591287b8
Vshepard879 <vshepard879@gmail.com>
2022-07-10 23:43:15 -0400

    issues with portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,37 +1,49 @@
 import React from 'react';
 import Project from '../Project';
 
-
+function Portfolio() {
 	const projects = [
 		{
-			name: 'Rabbit Hole',
+			name: 'Living Real',
 			description:
-				'This is a tech blog app that I built using handlebars express and Node.js. It is a simple blog app that allows users to create, edit, and delete posts. It also allows users to comment on posts. It is a single page application that is responsive to all screen sizes.',
+				'This is a property management system that is used by landlords viewing and managing their properties and for tenants to allow for maintenance requests and pay rent.',
 			image: 'living-real-image.png',
 			technologies: [
 				'HTML/CSS',
 				'JavaScript',
 				'Node.js',
 				'Express & NoSQL',
-				'Heroku'
+				'Heroku & AWS S3',
+				'React.js',
+				'MERN Stack',
 			],
-			github: 'https://github.com/beimy/tech-blog',
-			deployed: 'https://therabbithole-blog.herokuapp.com',
+			github: 'https://github.com/P3T2-Realty-Rogues/living-real',
+			deployed: 'https://living-real.herokuapp.com/',
 		},
 		{
-			name: 'E-Commerce Backend',
+			name: 'Auto Tracker',
 			description:
-				'A backend development application that creates a database for an e-commerce site that will utilize Object-Relational Mapping to interact with the database. The application is built using best practices from the RESTful CRUD operations concept. This is a command line application to manage an e-commerce database site.',
+				'This is your personal automobile information tracking system. Track time dependent, vehicle specific information from a single application, available on any web-enabled device.',
 			image: 'auto-tracker-image.png',
 			technologies: [
+				'HTML/CSS',
 				'JavaScript',
 				'Node.js',
-				'MYSQL2',
+				'Handlebars.js',
 				'Express & Sequelize',
-				'Nodemon',
+				'Heroku & AWS S3',
 			],
-			github: 'https://github.com/Vshepard879/E-commerce-Back-End',
-			deployed: 'Backend service with walk-through available on the GitHub repository.',
+			github: 'https://github.com/OwaisIslam/auto-tracker',
+			deployed: 'https://team-3-auto-tracker.herokuapp.com/',
+		},
+		{
+			name: 'Flash Beats',
+			description:
+				'A clean and accurate way to search for your favorite songs while also displaying the lyrics.',
+			image: 'flash-beats-image.gif',
+			technologies: ['HTML/CSS', 'JavaScript', 'jQuery'],
+			github: 'https://github.com/OwaisIslam/flash-beats',
+			deployed: 'https://teamflash1.github.io/music-app/',
 		},
 		{
 			name: 'Budget Tracker',
@@ -45,8 +57,8 @@ import Project from '../Project';
 				'Node.js',
 				'Express',
 			],
-			github: 'https://github.com/Vshepard879/PWA-Budget-Tracker',
-			deployed: ' https://pwa-budget-tracker-vshepard879.herokuapp.com/',
+			github: 'https://github.com/OwaisIslam/budget-tracker',
+			deployed: 'https://budget-tracker-owais.herokuapp.com/',
 		},
 		{
 			name: 'Weather Dashboard',
@@ -54,8 +66,8 @@ import Project from '../Project';
 				'A weather dashboard that shows current weather and five-day weather forecast for any city.',
 			image: 'weather-dashboard-image.png',
 			technologies: ['HTML/CSS', 'JavaScript'],
-			github: 'https://github.com/Vshepard879/Weather-Dashboard',
-			deployed: 'https://vshepard879.github.io/Weather-Dashboard/',
+			github: 'https://github.com/OwaisIslam/weather-dashboard',
+			deployed: 'https://owaisislam.github.io/weather-dashboard/',
 		},
 		{
 			name: 'Password Generator',
@@ -63,8 +75,8 @@ import Project from '../Project';
 				'This is an application that allows you to generate a password from 8 to 128 characters based on criteria you select.',
 			image: 'password-generator-image.png',
 			technologies: ['HTML/CSS', 'JavaScript'],
-			github: 'https://github.com/Vshepard879/Challenge-3-Password-Generator',
-			deployed: 'https://vshepard879.github.io/Challenge-3-Password-Generator/',
+			github: 'https://github.com/OwaisIslam/password-generator',
+			deployed: 'https://owaisislam.github.io/password-generator/',
 		},
 		{
 			name: 'Note Taker',
@@ -72,11 +84,11 @@ import Project from '../Project';
 				'This application allows the user to enter notes and as they save, they appear in the sidebar on the left. Notes can be deleted by clicking the trash can next to the corresponding note.',
 			image: 'note-taker-image.png',
 			technologies: ['HTML/CSS', 'JavaScript', 'Express.js'],
-			github: 'https://github.com/Vshepard879/Note-Taker',
-			deployed: 'https://note-taker-vshepard879.herokuapp.com/notes',
+			github: 'https://github.com/OwaisIslam/note-taker',
+			deployed: 'https://note-taker-owais.herokuapp.com/',
 		},
 	];
-	function Portfolio() {
+
 	return (
 		<section>
 			<div className="center">
